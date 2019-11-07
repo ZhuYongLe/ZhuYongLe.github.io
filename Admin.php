7c85f0a92127c28151e3613fe3487dad7201baa7
LEGEnd <43946094+ZhuYongLe@users.noreply.github.com>
2019-11-07 21:52:05 +0800

    修改了Admin.php
    tp5下的admin模块下的controller下的Admin控制器

--- Admin.php
+++ Admin.php
@@ -1,32 +1,106 @@
 <?php
 namespace app\admin\controller;
 
-use think\Controller;
+use app\admin\controller\WAF;
+use app\admin\model\Admin as modelAdmin;  //给model下的Admin类 起别名为modelAdmin
 
-// 公用控制器(防SQL注入和xss漏洞)
-class WAF extends Controller
-{    
-	public $filter;
-	public function __construct() {
-		$this->filter = "\\<.+javascript:window\\[.{1}\\\\x|<.*=(&#\\d+?;?)+?>|<.*(data|src)=data:text\\/html.*>|\\b(alert\\(|confirm\\(|expression\\(|prompt\\(|benchmark\s*?\(.*\)|sleep\s*?\(.*\)|\\b(group_)?concat[\\s\\/\\*]*?\\([^\\)]+?\\)|\bcase[\s\/\*]*?when[\s\/\*]*?\([^\)]+?\)|load_file\s*?\\()|<[a-z]+?\\b[^>]*?\\bon([a-z]{4,})\s*?=|^\\+\\/v(8|9)|\\b(and|or)\\b\\s*?([\\(\\)'\"\\d]+?=[\\(\\)'\"\\d]+?|[\\(\\)'\"a-zA-Z]+?=[\\(\\)'\"a-zA-Z]+?|>|<|\s+?[\\w]+?\\s+?\\bin\\b\\s*?\(|\\blike\\b\\s+?[\"'])|\\/\\*.*\\*\\/|<\\s*script\\b|\\bEXEC\\b|UNION.+?SELECT\s*(\(.+\)\s*|@{1,2}.+?\s*|\s+?.+?|(`|'|\").*?(`|'|\")\s*)|UPDATE\s*(\(.+\)\s*|@{1,2}.+?\s*|\s+?.+?|(`|'|\").*?(`|'|\")\s*)SET|INSERT\\s+INTO.+?VALUES|(SELECT|DELETE)@{0,2}(\\(.+\\)|\\s+?.+?\\s+?|(`|'|\").*?(`|'|\"))FROM(\\(.+\\)|\\s+?.+?|(`|'|\").*?(`|'|\"))|(CREATE|ALTER|DROP|TRUNCATE)\\s+(TABLE|DATABASE)|<.*(iframe|frame|style|embed|object|frameset|meta|xml|a|img)|hacker";
-	}
-	public function CheckInject($Value){
-		if(is_array($Value)){
-			$Value=implode($Value);
-		}
-		if (preg_match("/".$this->filter."/is",$Value)==1){
-			die("非法操作!");
-		}
-	}
-}
-$checkhack = new WAF();
-foreach($_GET as $key=>$value){
-	$checkhack->CheckInject($value);
-}
-foreach($_POST as $key=>$value){
-	$checkhack->CheckInject($value);
-}
-foreach($_COOKIE as $key=>$value){
-	$checkhack->CheckInject($value);
+class Admin extends WAF
+{
+    // 展示管理员列表
+    public function lst()
+    {
+        $admin = new modelAdmin();  //创建model下的Admin类的对象
 
+        $data = $admin->getAdmin();
+
+        $this->assign('adminRes',$data);
+
+        return $this->fetch();
+    }
+
+    // 增加管理员
+    public function add()
+    {
+        if (request()->isPost())
+        {
+            $admin = new modelAdmin();
+
+            $data = input('post.');  //获取POST方式提交的变量
+
+            $res = $admin->addAdmin($data);  //添加管理员
+
+            if ($res)
+            {
+                $this->success('添加管理员成功', url('lst'));  //添加成功，跳转到本控制器下lst页面
+            }
+            else
+            {
+                $this->error('添加管理员失败');  //添加失败，会自动跳转上一个页面
+            }
+        }
+    	return $this->fetch();
+    }
+
+    // 编辑管理员信息
+    public function edit($id)
+    {
+        $admin = new modelAdmin();
+
+        $oldData = $admin->getOneAdmin($id);  //用get提交的id进行查找 返回一个管理员信息
+
+        $this->assign('adminRes',$oldData);  //把包含$oldData的信息以变量adminMag传送给前台
+
+        if (request()->isPost())  //当数据提交方式为post时
+        {
+            $newData = input('post.');  //获取post提交的数据
+
+            if (!$newData['name'])  //新姓名为空时
+            {
+                $this->error('管理员姓名不得为空');
+            }
+
+            $res = $admin->editAdmin($oldData,$newData);  //修改管理员信息
+
+            if ($res)
+            {
+                $this->success('修改成功',url('lst'));
+            }
+            else 
+            {
+                if ($res == 0)
+                {
+                    $this->success('修改成功，密码不变',url('lst'));
+                }
+                else
+                {
+                    $this->error('修改失败');
+                }
+            }
+        }
+    	return $this->fetch();
+    }
+    
+    // 删除管理员
+    public function del($id)
+    {
+        $admin = new modelAdmin();
+
+        $res = $admin->delAdmin($id);
+
+        if ($res)
+        {
+            $this->success('删除成功',url('lst'));
+        }
+        else
+        {
+            $this->error('删除失败');
+        }
+    }
+
+    // 管理员退出登录
+    public function logout()
+    {
+        session(null);
+        $this->success('退出系统成功',url('Login/index'));
+    }
 }
